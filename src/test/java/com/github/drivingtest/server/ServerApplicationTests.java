30159d3f460fddd22fd5de05296e73b06ef3102e
mateuszjanczak <mateuszjanczak.mj@gmail.com>
2021-02-27 17:45:01 +0100

    Spring Boot & dependencies #1

---
+++ src/test/java/com/github/drivingtest/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.drivingtest.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
